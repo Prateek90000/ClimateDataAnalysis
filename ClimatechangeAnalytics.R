60ba489cd9dbda870f29f79bae83b1e68674ed96
github_Prateek90000 <pg21prateek.dadhich@isbm.ac.in>
2022-10-21 10:54:52 +0530

    climateanalysiscompleted

--- ClimatechangeAnalytics.R
+++ ClimatechangeAnalytics.R
@@ -5,7 +5,21 @@ ClimateData = read.csv("climate_change.csv")
 # Below all are equivalent commands
 ClimateTrain = subset(ClimateData, Year <=2006)
 ClimateTrain = subset(ClimateData,ClimateData$Year <=2006)
-filter(ClimateData, "Year" <2006)
+#filter(ClimateData, "Year" <2006)
 
 ClimateTest =subset(ClimateData,Year >2006)
 View(ClimateTest)
+
+#create a linear model - first model -R!
+climatelm =lm(Temp ~ MEI + CO2 + CH4 + N2O + CFC.11 + CFC.12 +TSI + Aerosols, data=ClimateTrain)
+
+summary(climatelm)
+
+#correlations
+cor(ClimateTrain)
+
+CorrelationAllVariable = cor(ClimateTrain)
+View(CorrelationAllVariable)
+
+LInReg = lm(Temp ~ MEI + N2O + TSI + Aerosols, data=ClimateTrain)
+summary(LInReg)
